ce74e6e78c0d98e7ceaf38eee9ede9276333934c
chae03yb <chae03yb@gmail.com>
2023-11-27 11:25:42 +0900

    디코드 함수 작성중, 반복 횟수 계산 수정 필요

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define MAX_LENGTH 10
 
@@ -8,10 +9,15 @@ char* decode(const char* string, char* result);
 
 int main() {
     char string[MAX_LENGTH];
-    char result[MAX_LENGTH];
+    char e_result[MAX_LENGTH];
+    char d_result[MAX_LENGTH];
     scanf("%s", string);
 
-    printf("%s", encode(string, result));
+    encode(string, e_result);
+    decode(e_result, d_result);
+
+    printf("%s\n", e_result);
+    printf("%s\n", d_result);
 
     return 0;
 }
@@ -49,5 +55,34 @@ char* encode(const char* string, char* result) {
 }
 
 char* decode(const char* string, char* result) {
+    int index = 0,
+        result_index = 0;
+    char selected_char;
 
+    while (string[index] != '\0') {  // 문자열 반복
+        bool is_repeated = '1' <= string[index+1] && string[index+1] <= '9';  // 다음 문자가 숫자면 반복됨
+        int repeat_count = 0;  // 반복 횟수 저장용
+        selected_char = string[index];  // 현재 문자
+
+        result[result_index++] = selected_char;  // 인덱스 증가하며 결과 문자열에 현재 문자 추가
+
+        if (is_repeated) {  // 반복되는 문자열이라면
+            char buff[3];
+            for (int i=2;i>=0;i--) {  // 반복 횟수 셈
+                // FIXME: 연산으로 반복 횟수 세어야 함
+                if ('0' <= string[index+i] && string[index+i] <= '9')
+                    buff[i] = string[index+i];
+                else
+                    continue;
+            }
+            sscanf(buff, "%d", &repeat_count);
+
+            for (int i=0;i<repeat_count;i++)  // 반복 횟수만큼 문자 추가
+                result[result_index++] = selected_char;
+        }
+        index++;
+    }
+    result[result_index] = '\0';
+
+    return result;
 }
